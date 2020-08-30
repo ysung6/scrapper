5be766882218e7aa0522be389710b3f72eeb86ec
ysung6 <ysung5140@gmail.com>
2020-08-31 00:10:19 +0900

    using coursera example. Need to fix from docs.

--- example.go
+++ example.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	//"encoding/csv"
+	"encoding/csv"
 	"log"
 	"os"
 
-	//"github.com/gocolly/colly"
+	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -16,4 +16,26 @@ func main() {
 		return
 	}
 	defer file.Close()
+	writer := csv.NewWriter(file)
+
+	// csv header
+	writer.Write([]string{"Name", "Symbol", "Price (USD)", "Volume (USD)", "Market capacity (USD)", "Change (1h)", "Change (24h)", "Change (7d)"})
+
+	c := colly.NewCollector()
+	c.OnHTML("tbody tr", func(e *colly.HTMLElement){
+		writer.Write([]string{
+			e.ChildText(".currency-name-container"),
+			e.ChildText(".col-symbol"),
+			e.ChildAttr("a.price", "data-usd"),
+			e.ChildAttr("a.volume", "data-usd"),
+			e.ChildAttr(".market-cap", "data-usd"),
+			e.ChildText(".percent-1h"),
+			e.ChildText(".percent-24h"),
+			e.ChildText(".percent-7d"),
+		})
+	})
+
+	c.Visit("https://coinmarketcap.com/all/views/all/")
+
+	log.Printf("Scraping done. Check file %q for results\n", fName)
 }
